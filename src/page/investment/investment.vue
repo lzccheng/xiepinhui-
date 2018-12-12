d5d46e4ba6f6ea093c5de853a5b06dde893827ff
zstop <582990478@qq.com>
2018-12-12 16:00:33 +0800

    my

---
+++ src/page/investment/investment.vue
@@ -0,0 +1,33 @@
+<style scoped lang="less">
+
+</style>
+<template>
+    <div class="page">
+        <div>
+            招商合作
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+    }
+}
+</script>
